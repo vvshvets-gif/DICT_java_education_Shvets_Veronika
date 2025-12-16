c567cd64fd1531322f1e12c7ac554c351da60da6
Veronika <v.v.shvets@student.khai.edu>
2025-12-16 14:02:17 +0200

    added one more print to Test.java on the feature branch

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,7 @@
 public class Test {
     public static void main(String[] args) {
-    System.out.println("education");
-    //comment
-        System.out.println('DICT')
-}
+        System.out.println("education");
+        //comment
+        System.out.println("DICT");
+    }
 }
