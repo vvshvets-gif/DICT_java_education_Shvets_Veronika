5d2c80562be7f3bf39cfc78033746656364758cb
Veronika <v.v.shvets@student.khai.edu>
2025-12-18 22:49:12 +0200

    Stages 2-3: Add word list and random selection

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,8 +1,20 @@
 package Hangman;
+import java.util.*;
 
 public class Hangman {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("HANGMAN");
-        System.out.println("The game will be available soon.");
+        String[] words = {"python", "java", "javascript", "kotlin"};
+        String targetWord = words[new Random().nextInt(words.length)];
+
+        System.out.print("Guess the word: > ");
+        String guess = scanner.nextLine();
+
+        if (guess.equals(targetWord)) {
+            System.out.println("You survived!");
+        } else {
+            System.out.println("You lost!");
+        }
     }
 }
