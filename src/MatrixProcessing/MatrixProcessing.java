5f27104b05be92eecf2ed21688c72f018ad9fb6d
Veronika <v.v.shvets@student.khai.edu>
2025-12-18 23:25:21 +0200

    Stage 5: Determinant calculation implemented using recursion

--- src/MatrixProcessing/MatrixProcessing.java
+++ src/MatrixProcessing/MatrixProcessing.java
@@ -1,4 +1,5 @@
 package MatrixProcessing;
+
 import java.util.Scanner;
 
 class Matrix {
@@ -12,55 +13,34 @@ class Matrix {
         this.data = new double[rows][cols];
     }
 
-    public void setElement(int r, int c, double val) { data[r][c] = val; }
-    public double getElement(int r, int c) { return data[r][c]; }
+    public void setElement(int r, int c, double val) {
+        data[r][c] = val;
+    }
+
     public int getRows() { return rows; }
     public int getCols() { return cols; }
 
-    public Matrix add(Matrix other) {
-        if (this.rows != other.rows || this.cols != other.cols) return null;
-        Matrix result = new Matrix(rows, cols);
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                result.setElement(i, j, this.data[i][j] + other.data[i][j]);
-            }
+    public double determinant() {
+        if (this.rows != this.cols) {
+            return 0; // Визначник лише для квадратних матриць
         }
-        return result;
+        return calculateDeterminant(this.data);
     }
 
-    public Matrix multiply(double constant) {
-        Matrix result = new Matrix(rows, cols);
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                result.setElement(i, j, this.data[i][j] * constant);
-            }
+
+    private double calculateDeterminant(double[][] matrix) {
+        int n = matrix.length;
+
+        if (n == 1) return matrix[0][0];
+
+        if (n == 2) {
+            return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
         }
-        return result;
+
+        double det = 0;
+        for (int j = 0; j < n; j++) {
+            det += Math.pow(-1, j) * matrix[0][j] * calculateDeterminant(getMinor(matrix, 0, j));
+        }
+        return det;
     }
 
-    public void print() {
-        for (double[] row : data) {
-            for (double val : row) {
-                // Форматування для гарного виводу
-                if (val == (long) val) System.out.print((long)val + " ");
-                else System.out.printf("%.2f ", val);
-            }
-            System.out.println();
-        }
-    }
-
-    public Matrix multiply(Matrix other) {
-        if (this.cols != other.rows) return null;
-        Matrix result = new Matrix(this.rows, other.cols);
-        for (int i = 0; i < this.rows; i++) {
-            for (int j = 0; j < other.cols; j++) {
-                double sum = 0;
-                for (int k = 0; k < this.cols; k++) {
-                    sum += this.data[i][k] * other.data[k][j];
-                }
-                result.setElement(i, j, sum);
-            }
-        }
-        return result;
-    }
-}
