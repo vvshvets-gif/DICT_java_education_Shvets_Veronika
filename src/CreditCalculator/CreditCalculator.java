2b7b7f24126b8e38fe8bc67cbd4b8c29899f3b8c
Veronika <v.v.shvets@student.khai.edu>
2025-12-19 00:00:07 +0200

    Stage 1: Basic output setup

---
+++ src/CreditCalculator/CreditCalculator.java
@@ -0,0 +1,11 @@
+package CreditCalculator;
+
+public class CreditCalculator {
+    public static void main(String[] args) {
+        System.out.println("Loan principal: 1000");
+        System.out.println("Month 1: repaid 250");
+        System.out.println("Month 2: repaid 250");
+        System.out.println("Month 3: repaid 500");
+        System.out.println("The loan has been repaid!");
+    }
+}
