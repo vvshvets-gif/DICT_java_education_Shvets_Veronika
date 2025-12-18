5bd88d65ddd9719cae22f927290984a2bf8de33b
Veronika <v.v.shvets@student.khai.edu>
2025-12-18 21:44:48 +0200

    ChatBot 1-st stage

---
+++ src/ChatBot/ChatBot.java
@@ -0,0 +1,12 @@
+package ChatBot;
+
+public class ChatBot {
+    public static void main(String[] args) {
+        SendMessage("Hello! My name is PR_4_bot.");
+        SendMessage("I was created in 2025.");
+    }
+
+    private static void SendMessage(String message) {
+        System.out.println(message);
+    }
+}
