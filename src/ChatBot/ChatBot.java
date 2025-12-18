98745da3bfbf3abd2d8d17cc2c7f19ed2b30699f
Veronika <v.v.shvets@student.khai.edu>
2025-12-18 21:53:39 +0200

    ChatBot 2 stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,9 +1,16 @@
+
 package ChatBot;
+
+import java.util.Scanner;
 
 public class ChatBot {
     public static void main(String[] args) {
         SendMessage("Hello! My name is PR_4_bot.");
         SendMessage("I was created in 2025.");
+        SendMessage("Please, remind me your name.");
+        var scanner = new Scanner(System.in);
+        var name = scanner.nextLine();
+        SendMessage(String.format("What a great name you have, %s!", name));
     }
 
     private static void SendMessage(String message) {
