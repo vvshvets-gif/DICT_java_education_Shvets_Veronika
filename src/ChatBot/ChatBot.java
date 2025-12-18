f0e9cdd409fc2d6a37dd7ad61df61be3cb1b9b6c
Veronika <v.v.shvets@student.khai.edu>
2025-12-18 21:59:49 +0200

    ChatBot 3 stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -11,9 +11,24 @@ public class ChatBot {
         var scanner = new Scanner(System.in);
         var name = scanner.nextLine();
         SendMessage(String.format("What a great name you have, %s!", name));
+        SendMessage("Let me guess your age.");
+        SendMessage("Enter remainders of dividing your age by 3, 5 and 7.");
+        var age1 = scanner.nextInt();
+        var age2 = scanner.nextInt();
+        var age3 = scanner.nextInt();
+        var age = CalculateAge(age1, age2, age3);
+        SendMessage(String.format("Your age is %d; that's a good time to start programming!", age));
     }
 
     private static void SendMessage(String message) {
         System.out.println(message);
+    }
+
+    private static int CalculateAge(int age1, int age2, int age3) {
+        var remainder3 = age1 % 3;
+        var remainder5 = age2 % 5;
+        var remainder7 = age3 % 7;
+        var age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+        return age;
     }
 }
