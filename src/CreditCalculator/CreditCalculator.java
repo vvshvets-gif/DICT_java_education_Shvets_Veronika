27b337c923c26d5e944a29096c9b28afaee8bb42
Veronika <v.v.shvets@student.khai.edu>
2025-12-19 00:06:53 +0200

    Stage 3: Annuity payment formulas and time formatting

--- src/CreditCalculator/CreditCalculator.java
+++ src/CreditCalculator/CreditCalculator.java
@@ -6,41 +6,71 @@ public class CreditCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // 1. Просимо ввести основну суму
-        System.out.println("Enter the loan principal:");
-        int principal = scanner.nextInt();
-
-        // 2. Вибір дії
         System.out.println("What do you want to calculate?");
-        System.out.println("type \"m\" – for number of monthly payments,");
-        System.out.println("type \"p\" – for the monthly payment:");
+        System.out.println("type \"n\" for number of monthly payments,");
+        System.out.println("type \"a\" for annuity monthly payment amount,");
+        System.out.println("type \"p\" for loan principal:");
         String action = scanner.next();
 
-        if (action.equals("m")) {
-            // РОЗРАХУНОК КІЛЬКОСТІ МІСЯЦІВ
+        if (action.equals("n")) {
+            System.out.println("Enter the loan principal:");
+            double p = scanner.nextDouble();
             System.out.println("Enter the monthly payment:");
-            int payment = scanner.nextInt();
+            double a = scanner.nextDouble();
+            System.out.println("Enter the loan interest:");
+            double interest = scanner.nextDouble();
 
-            // Формула: principal / payment (округлення вгору)
-            int months = (int) Math.ceil((double) principal / payment);
-            System.out.println("It will take " + months + " months to repay the loan");
+            double i = interest / (12 * 100);
+            double n = Math.ceil(Math.log(a / (a - i * p)) / Math.log(1 + i));
+
+            printTime((int) n);
+
+        } else if (action.equals("a")) {
+            System.out.println("Enter the loan principal:");
+            double p = scanner.nextDouble();
+            System.out.println("Enter the number of periods:");
+            int n = scanner.nextInt();
+            System.out.println("Enter the loan interest:");
+            double interest = scanner.nextDouble();
+
+            double i = interest / (12 * 100);
+            double pow = Math.pow(1 + i, n);
+            double a = Math.ceil(p * (i * pow) / (pow - 1));
+
+            System.out.println("Your monthly payment = " + (int) a + "!");
 
         } else if (action.equals("p")) {
-            // РОЗРАХУНОК ЩОМІСЯЧНОГО ПЛАТЕЖУ
-            System.out.println("Enter the number of months:");
-            int periods = scanner.nextInt();
+            System.out.println("Enter the annuity payment:");
+            double a = scanner.nextDouble();
+            System.out.println("Enter the number of periods:");
+            int n = scanner.nextInt();
+            System.out.println("Enter the loan interest:");
+            double interest = scanner.nextDouble();
 
-            // Рахуємо платіж з округленням вгору
-            int payment = (int) Math.ceil((double) principal / periods);
+            double i = interest / (12 * 100);
+            double pow = Math.pow(1 + i, n);
+            double p = Math.floor(a / ((i * pow) / (pow - 1)));
 
-            // Рахуємо останній платіж за формулою з ТЗ
-            int lastPayment = principal - (periods - 1) * payment;
-
-            if (lastPayment != payment && lastPayment > 0) {
-                System.out.println("Your monthly payment = " + payment + " and the last payment = " + lastPayment + ".");
-            } else {
-                System.out.println("Your monthly payment = " + payment);
-            }
+            System.out.println("Your loan principal = " + (int) p + "!");
         }
+    }
+
+    private static void printTime(int totalMonths) {
+        int years = totalMonths / 12;
+        int months = totalMonths % 12;
+
+        String result = "It will take ";
+        if (years > 0) {
+            result += years + (years == 1 ? " year" : " years");
+        }
+        if (years > 0 && months > 0) {
+            result += " and ";
+        }
+        if (months > 0) {
+            result += months + (months == 1 ? " month" : " months");
+        }
+        result += " to repay this loan!";
+
+        System.out.println(result);
     }
 }
